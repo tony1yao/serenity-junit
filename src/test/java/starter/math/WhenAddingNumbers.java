027d39c4a67fb5b107aa720e83859dba3fb7fd7e
Tony Yao <yaoziliang@163.com>
2020-04-20 20:17:57 +1000

    first commit

---
+++ src/test/java/starter/math/WhenAddingNumbers.java
@@ -0,0 +1,30 @@
+package starter.math;
+
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.thucydides.core.annotations.Narrative;
+import net.thucydides.core.annotations.Pending;
+import net.thucydides.core.annotations.Steps;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import starter.steps.MathWizSteps;
+
+@RunWith(SerenityRunner.class)
+@Narrative(text={"Maths is important."})
+public class WhenAddingNumbers {
+
+    @Steps
+    MathWizSteps michael;
+
+    @Test
+    @Pending
+    public void addingSums() {
+        // Given
+        michael.startsWith(1);
+
+        // When
+        michael.adds(2);
+
+        // Then
+        michael.shouldHave(3);
+    }
+}
